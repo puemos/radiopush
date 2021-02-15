45e6d1c80d09fbaab53db974916dcffc0dcf5103
Shy <puemos@gmail.com>
2021-02-15 21:09:39 +0100

    remove: giga stuff

--- config/prod.exs
+++ config/prod.exs
@@ -53,16 +53,3 @@ config :logger, level: :info
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
 import_config "prod.secret.exs"
-
-config :radiopush, RadiopushWeb.Endpoint,
-  # Possibly not needed, but doesn't hurt
-  http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  server: true
-
-config :radiopush, Radiopush.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL"),
-  ssl: true,
-  pool_size: 2
