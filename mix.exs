be8d015a3cd145b6ec99a1d54c2113a77bd32142
Shy <puemos@gmail.com>
2021-02-15 20:49:20 +0100

    change: elixir 1.11.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Radiopush.MixProject do
     [
       app: :radiopush,
       version: "0.1.0",
-      elixir: "~> 1.10.1",
+      elixir: "~> 1.11.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
