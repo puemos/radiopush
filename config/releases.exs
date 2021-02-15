fa9f0a2573689d3052c00011c6e998a114c201a6
Shy <puemos@gmail.com>
2021-02-15 22:49:24 +0100

    releases configuration

--- config/releases.exs
+++ config/releases.exs
@@ -2,6 +2,5 @@ import Config
 
 config :radiopush, RadiopushWeb.Endpoint,
   server: true,
-  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
   http: [port: {:system, "PORT"}],
   url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
