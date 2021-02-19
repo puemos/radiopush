a5b0eb3738f62de1f46882ac0819a1c294255603
Shy <puemos@gmail.com>
2021-02-19 09:03:33 +0100

    fix: downgrade elixir to 1.11.x

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Radiopush.MixProject do
     [
       app: :radiopush,
       version: "0.1.0",
-      elixir: "~> 1.11.2",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
