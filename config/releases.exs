4aa6f96aecc94197de70ad3f605aef1add946ef1
Shy <puemos@gmail.com>
2021-02-22 23:21:06 +0100

    add: prod db ssl off

--- config/releases.exs
+++ config/releases.exs
@@ -12,7 +12,7 @@ database_url =
     """
 
 config :radiopush, Radiopush.Repo,
-  ssl: true,
+  # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
